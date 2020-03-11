63286ac53a9b8361c30343c9a57dd6d53665a76e
EmmOriold <EmmOriold@Gmail.com>
2020-03-11 19:55:27 -0400

    Antlr
    Hopefully all of the Antlr stuff

---
+++ testharness.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+declare var require:any;
+let fs = require("fs");
+import {parse} from "./parser";
+
+function main(){
+    let inp = fs.readFileSync("input1.txt","utf8");
+    let root = parse( inp );
+    fs.writeFileSync( "tree.dot", root );
+    console.log("Wrote tree.dot");
+    try{
+        inp = fs.readFileSync("input2.txt","utf8");
+        root = parse( inp );
+        console.log("Accepted invalid input");
+    } catch(e){
+        console.log("Rejected invalid input. Good.");
+    }
+}
+
+
+main()
